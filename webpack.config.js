d3285932c4a9259dd0e65a7f3e201feb885909b8
Antonio Ciccarone <antoniociccarone@233-US-M.trustyou.local>
2019-10-06 20:52:13 -0400

    push quick pug template for future use

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+// webpack.config.js
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+isDevelopment = true;
+const pug = {
+  test: /\.pug$/,
+  use: ['html-loader?attrs=false', 'pug-html-loader']
+};
+const config = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      pug,
+      {
+      test: /\.module\.s(a|c)ss$/,
+      loader: [
+        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]',
+            camelCase: true,
+            sourceMap: isDevelopment
+          }
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: isDevelopment
+          }
+        }
+      ]
+    },
+    {
+      test: /\.s(a|c)ss$/,
+      exclude: /\.module.(s(a|c)ss)$/,
+      loader: [
+        isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: isDevelopment
+          }
+        }
+      ]
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.pug',
+      inject: false
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    })
+ ]
+};
+module.exports = config;
